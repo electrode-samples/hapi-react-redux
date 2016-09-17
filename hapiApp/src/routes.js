1199d773f69ab02477cf210e8cd5fbfa70b9a975
raydecastro <captray108@gmail.com>
2016-09-16 21:12:53 -0700

    docs. added home page to match other sample apps.

--- hapiApp/src/routes.js
+++ hapiApp/src/routes.js
@@ -3,10 +3,12 @@ import { Router, Route } from 'react-router';
 import SSRCachingTemplateType from "./components/SSRCachingTemplateType";
 import SSRCachingSimpleType from "./components/SSRCachingSimpleType";
 import AboveFold from "./components/above-the-fold";
+import Home from "./components/home";
 
 module.exports = (
 	<Router>
 		<Route>
+			<Route path="/" component={Home} />
 			<Route path="/above-the-fold" component={AboveFold} />
 			<Route path="/ssrcachingtemplatetype" component={SSRCachingTemplateType} />
 			<Route path="/ssrcachingsimpletype" component={SSRCachingSimpleType} />
